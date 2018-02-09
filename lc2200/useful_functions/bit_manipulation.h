139b34d22e293db86070b2d936a4fb786ae23557
Thomas Peacemaker <twpeacemaker@mail.roanoke.edu>
2018-02-08 22:30:21 -0500

    new prog

--- lc2200/useful_functions/bit_manipulation.h
+++ lc2200/useful_functions/bit_manipulation.h
@@ -9,7 +9,7 @@
 //POST: returns the mask with ones from the left index to the right index
 //      if there is no errors, if the function throws an error the function
 //      return garbage data;
-uint getMaskBit(int left_index, int right_index) ;
+uint getMaskBit(int left_index, int right_index);
 
 //PRE:  @param int num: # which you want to get a sub-bit for
 //      @param int left_index: [right_index-0]the left most index of the sub-bit
@@ -17,11 +17,11 @@ uint getMaskBit(int left_index, int right_index) ;
 //             sub-bit
 //      the left and right indexes are inclusive and this function will not
 //      changen int num.
-//      @param bool check_for_neg optional, tells the program to check for neg
+//      @param bool check_for_neg (optional), tells the program to check for neg
 //POST: will return the sub-bit of int num from left index to right index
 //      inclusive if no errors, if errors will return garbage data
 uint getBits(uint num, int left_index, int right_index,
-             bool check_for_neg);
+             bool check_for_neg = false);
 
 
 //PRE:  @param int byte_num, [1-4]
